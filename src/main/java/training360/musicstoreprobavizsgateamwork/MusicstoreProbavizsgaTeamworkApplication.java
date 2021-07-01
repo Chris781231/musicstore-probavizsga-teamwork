99a1c8d55bf0866daa5c162f34348819128cac56
LigetiKaroly <ligeti.karoly78@gmail.com>
2021-07-01 12:39:16 +0200

    Init commit.

---
+++ src/main/java/training360/musicstoreprobavizsgateamwork/MusicstoreProbavizsgaTeamworkApplication.java
@@ -0,0 +1,13 @@
+package training360.musicstoreprobavizsgateamwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicstoreProbavizsgaTeamworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicstoreProbavizsgaTeamworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/training360/musicstoreprobavizsgateamwork/MusicstoreProbavizsgaTeamworkApplicationTests.java
@@ -0,0 +1,13 @@
+package training360.musicstoreprobavizsgateamwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicstoreProbavizsgaTeamworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
